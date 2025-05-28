5e9a4616a0c9ddcf198a6358c29622e37c75dc96
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-05-27 21:18:03 -0300

    ✨ Update TypeScript configuration and enhance Vite alias resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { federation } from '@module-federation/vite';
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -16,5 +17,20 @@ export default defineConfig({
   ],
   server: {
     port: 3002,
+  },
+  resolve: {
+    alias: {
+      '@domain': path.resolve(__dirname, 'src/domain'),
+      '@fb': path.resolve(__dirname, 'src/firebase'),
+      '@usecases': path.resolve(__dirname, 'src/usecases'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@App': path.resolve(__dirname, 'src/App'),
+      '@generalTypes': path.resolve(__dirname, 'src/types'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+    },
+  },
+  optimizeDeps: {
+    exclude: ['@phosphor-icons/react'],
   },
 })
