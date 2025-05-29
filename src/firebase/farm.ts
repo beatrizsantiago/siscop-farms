b8a711b7889228854ffe326b9ccd9dda9f01f866
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2025-05-29 20:50:10 -0300

    ✨ Refactor FirebaseFarm to store product references in add and update methods

--- src/firebase/farm.ts
+++ src/firebase/farm.ts
@@ -1,6 +1,7 @@
 import { FarmRepository } from '@domain/repositories/FarmRepository';
 import {
   addDoc, collection, deleteDoc, doc,
+  DocumentReference,
   endAt, getDocs, orderBy, query,
   startAt, updateDoc,
 } from 'firebase/firestore';
@@ -10,10 +11,14 @@ import { firestore } from './config';
 
 class FirebaseFarm implements FarmRepository {
   async add(farm: Farm): Promise<Farm> {
+    const productsRefs = farm.available_products.map((product) =>
+      doc(firestore, 'products', product)
+    );
+
     const response = await addDoc(collection(firestore, 'farms'), {
       name: farm.name,
       geolocation: farm.geolocation,
-      available_products: farm.available_products,
+      available_products: productsRefs,
     });
 
     const newFarm = {
@@ -28,19 +33,35 @@ class FirebaseFarm implements FarmRepository {
     const farmsQuery = query(collection(firestore, 'farms'));
     const snapshot = await getDocs(farmsQuery);
 
-    const farms = snapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    } as Farm));
+    const farms: Farm[] = snapshot.docs.map((docSnapshot) => {
+      const data = docSnapshot.data();
+
+      const productRefs = Array.isArray(data.available_products)
+        ? data.available_products.filter((ref) => ref && typeof ref === 'object' && 'id' in ref)
+        : [];
+
+      const productIds = productRefs.map((ref: DocumentReference) => ref.id);
+
+      return {
+        id: docSnapshot.id,
+        name: data.name,
+        geolocation: data.geolocation,
+        available_products: productIds,
+      } as Farm; 
+    });
 
     return farms;
   }
 
   async update(farm: Farm): Promise<Farm> {
+    const productsRefs = farm.available_products.map((product) =>
+      doc(firestore, 'products', product)
+    );
+
     await updateDoc(doc(firestore, 'farms', farm.id), {
       name: farm.name,
       geolocation: farm.geolocation,
-      available_products: farm.available_products,
+      available_products: productsRefs,
     });
 
     return farm;
